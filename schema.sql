e0af9d56f333375efdd8528b9caeb04db2d9faa2
Nick Pettis <pettisnick@gmail.com>
2020-09-02 16:09:23 -0400

    creating tables

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,4) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL AUTO_INCREMENT,      -- INT to hold reference to role employee
+  manager_id INT NULL,                  -- INT to hold reference to another employee that manager of the current employee. May be NULL if the employee has no manager.
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
